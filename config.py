cd4e6e55e447caa5f167c2e460967b33f4d8d015
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-19 16:37:40 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config:
 
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL?sslmode=require")
 
 
 
