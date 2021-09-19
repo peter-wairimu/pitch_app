bde76ffd9a641cd074c110ef18551aa06966cc50
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-19 22:50:11 +0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringa:wairimu22@localhost/pitch'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'Heroku_Database_URL'
     app.config['QLALCHEMY_TRACK_MODIFICATIONS']= False
     db = SQLAlchemy(app)
     
